31647797e3c6871c7e7d573601a493c355f7ed2c
Gunars Abeltins <gunars2003@gmail.com>
2022-10-12 15:33:42 +0300

    Add functions

--- C7/C7.cpp
+++ C7/C7.cpp
@@ -11,6 +11,31 @@
 
 using std::cin, std::cout, std::endl;
 
+int kadanesAlgorithm(vector <int> &Vec, int &Len){ 
+   int max_so_far = Vec[0]; 
+   int curr_max = Vec[0]; 
+   for(int i = 1; i < Len; ++i){ 
+        curr_max = max(Vec[i], curr_max + Vec[i]); 
+        max_so_far = max(max_so_far, curr_max); 
+   } 
+   return max_so_far; 
+} 
+int maxRectangleSum(vector< vector<int> > &Matrix){
+    int lenY = (int)Matrix.size();
+    int lenX = (int)Matrix.front().size();
+    int res = -INF;
+    for(int top = 0; top < lenY; ++top){
+        vector <int> CurrRowSum(lenY, 0);
+        for(int bottom = top; bottom < lenY; ++bottom){
+            for(int i = 0; i < lenX; ++i){
+                CurrRowSum[i] += Matrix[bottom][i];
+            }
+            res = max(res, KadanesAlgorithm(CurrRowSum, lenX));
+        } 
+    }
+    return res;
+}
+
 int main()
 {
 
