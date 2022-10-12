ce69af6c61001d923d02b54bf87d696250797c6a
Gunars Abeltins <gunars2003@gmail.com>
2022-10-12 15:33:11 +0300

    Create boilerplate

---
+++ C7/C7.cpp
@@ -0,0 +1,18 @@
+// C7. Dots masīvs A(m,n), kas sastāv no veseliem skaitļiem. Atrast četrus 
+// skaitļus h,i,j,k, kas apzīmē apakšmatricu, kuras elementu kopsumma ir
+// vislielākā (h,i apzīmē apakšmatricas kreiso augšējo stūri,
+// bet j,k - labo apakšējo).
+
+// Gunārs Ābeltiņš
+// ga22008
+// 2022.10.11
+
+#include <iostream>
+
+using std::cin, std::cout, std::endl;
+
+int main()
+{
+
+    return 0;
+}
