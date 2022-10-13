332e9ca2be8baebc121efe3e69a7c4c288d1c7d4
Gunars Abeltins <gunars2003@gmail.com>
2022-10-13 22:27:58 +0300

    Switch from vectors to arrays

--- C7/C7.cpp
+++ C7/C7.cpp
@@ -1,4 +1,4 @@
-// C7. Dots masīvs A(m,n), kas sastāv no veseliem skaitļiem. Atrast četrus 
+// C7. Dots masīvs A(m,n), kas sastāv no veseliem skaitļiem. Atrast četrus
 // skaitļus h,i,j,k, kas apzīmē apakšmatricu, kuras elementu kopsumma ir
 // vislielākā (h,i apzīmē apakšmatricas kreiso augšējo stūri,
 // bet j,k - labo apakšējo).
@@ -8,36 +8,58 @@
 // 2022.10.11
 
 #include <iostream>
+#define INF 0x3f3f3f3f
 
-using std::cin, std::cout, std::endl;
+using std::cin, std::cout, std::endl, std::max;
 
-int kadanesAlgorithm(vector <int> &Vec, int &Len){ 
-   int max_so_far = Vec[0]; 
-   int curr_max = Vec[0]; 
-   for(int i = 1; i < Len; ++i){ 
-        curr_max = max(Vec[i], curr_max + Vec[i]); 
-        max_so_far = max(max_so_far, curr_max); 
-   } 
-   return max_so_far; 
-} 
-int maxRectangleSum(vector< vector<int> > &Matrix){
-    int lenY = (int)Matrix.size();
-    int lenX = (int)Matrix.front().size();
+int kadanesAlgorithm(int *Arr, int &Len)
+{
+    int max_so_far = Arr[0];
+    int curr_max = Arr[0];
+    for (int i = 1; i < Len; ++i)
+    {
+        curr_max = max(Arr[i], curr_max + Arr[i]);
+        max_so_far = max(max_so_far, curr_max);
+    }
+    return max_so_far;
+}
+
+int maxRectangleSum(int **Matrix, int &lenY, int &lenX)
+{
     int res = -INF;
-    for(int top = 0; top < lenY; ++top){
-        vector <int> CurrRowSum(lenY, 0);
-        for(int bottom = top; bottom < lenY; ++bottom){
-            for(int i = 0; i < lenX; ++i){
+    for (int top = 0; top < lenY; ++top)
+    {
+        int CurrRowSum[lenY]{};
+        for (int bottom = top; bottom < lenY; ++bottom)
+        {
+            for (int i = 0; i < lenX; ++i)
+            {
                 CurrRowSum[i] += Matrix[bottom][i];
             }
-            res = max(res, KadanesAlgorithm(CurrRowSum, lenX));
-        } 
+            res = max(res, kadanesAlgorithm(CurrRowSum, lenX));
+        }
     }
     return res;
 }
 
 int main()
 {
+    int side_len;
+    cin >> side_len;
+    int **Matrix;
+    Matrix = new int *[side_len];
+    for (int i = 0; i < side_len; i++)
+    {
+        Matrix[i] = new int[10];
+    }
 
+    for (int i = 0; i < side_len; ++i)
+    {
+        for (int j = 0; j < side_len; ++j)
+        {
+            cin >> Matrix[i][j];
+        }
+    }
+    cout << maxRectangleSum(Matrix, side_len, side_len);
     return 0;
 }
